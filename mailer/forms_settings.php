35fc95abc44bde3bd71aef4dedaebb46d51bc423
parth-khimji <parthkhimji@gmail.com>
2024-08-06 04:17:38 +0400

    Add files via upload

---
+++ mailer/forms_settings.php
@@ -0,0 +1,8 @@
+<?php
+	/* Forms settings */
+	$subj = "New message from 'Inspired Touch-Up'"; //letter subject
+	$to = 'parth@udaykhimjigroup.com'; // Enter Your E-mail
+	$from = 'salesm@inspire-d.me'; // Admin e-mail
+	$fromName = 'I:T'; // Your company name
+	$charset = 'UTF-8';
+?>
